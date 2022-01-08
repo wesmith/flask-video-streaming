e57142d8c87146b9aed8c318bd9f9db4340558e8
wesmith <smiwarsky@gmail.com>
2022-01-07 20:33:02 -0700

    ran successfully first try, some adjustment to comments, changed frame size

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+# WESmith 01/07/22 this worked with the simulated camera out
+# of the box: impressive
+
+# WS TODO
+# - run webcam via opencv
+# - add command-line inputs: camera type, url, port
+
 from importlib import import_module
 import os
 from flask import Flask, render_template, Response
 
 # import camera driver
 if os.environ.get('CAMERA'):
-    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+    Camera = import_module('camera_' +\
+                           os.environ['CAMERA']).Camera
 else:
     from camera import Camera
 
@@ -14,26 +23,27 @@ else:
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     """Video streaming home page."""
     return render_template('index.html')
-
 
 def gen(camera):
     """Video streaming generator function."""
     yield b'--frame\r\n'
     while True:
         frame = camera.get_frame()
-        yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
-
+        yield b'Content-Type: image/jpeg\r\n\r\n' + frame +\
+              b'\r\n--frame\r\n'
 
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    """
+    Video streaming route. Put this in the src attribute of an
+    img tag.
+    """
+    txt = 'multipart/x-mixed-replace; boundary=frame'
+    return Response(gen(Camera()), mimetype=txt)
 
 
 if __name__ == '__main__':
--- camera_opencv.py
+++ camera_opencv.py
@@ -8,7 +8,8 @@ class Camera(BaseCamera):
 
     def __init__(self):
         if os.environ.get('OPENCV_CAMERA_SOURCE'):
-            Camera.set_video_source(int(os.environ['OPENCV_CAMERA_SOURCE']))
+            val = int(os.environ['OPENCV_CAMERA_SOURCE']) # WS
+            Camera.set_video_source(val) # WS: shorten line
         super(Camera, self).__init__()
 
     @staticmethod
@@ -20,6 +21,11 @@ class Camera(BaseCamera):
         camera = cv2.VideoCapture(Camera.video_source)
         if not camera.isOpened():
             raise RuntimeError('Could not start camera.')
+
+        # sizes: 640x480, 352x288, 320x240, 176x144, 160x120
+        # need to verify and get a complete list WS
+        camera.set(cv2.CAP_PROP_FRAME_WIDTH,  640)  # WS
+        camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)  # WS
 
         while True:
             # read current frame
