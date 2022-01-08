c2940ab5bdb9056cefc591ee2e21fdcaa1d731b5
wesmith <smiwarsky@gmail.com>
2022-01-07 21:30:50 -0700

    formatting

--- camera.py
+++ camera.py
@@ -3,8 +3,10 @@ from base_camera import BaseCamera
 
 
 class Camera(BaseCamera):
-    """An emulated camera implementation that streams a repeated sequence of
-    files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
+    """
+    An emulated camera implementation that streams a repeated sequence
+    of files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second.
+    """
     imgs = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
 
     @staticmethod
