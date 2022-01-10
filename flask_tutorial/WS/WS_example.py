23d470457b9550b732947f7bb64aac691e2d3040
wesmith <smiwarsky@gmail.com>
2022-01-10 04:47:54 -0700

    successfully communicating 3x2 table of button selection to main app: ready for streaming video app

--- flask_tutorial/WS/WS_example.py
+++ flask_tutorial/WS/WS_example.py
@@ -9,13 +9,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-msg = {'on': 1, 'off': 0}
+buttons = {'INVERT': {'on': 0, 'off': 1},
+           'DEFOCUS':{'on': 2, 'off': 3},
+           'FLIP':   {'on': 4, 'off': 5}}
+
+class Message():
+    def __init__(self):
+        self.mapping = {'0': 'INVERT ON',  '1': 'INVERT OFF',
+                        '2': 'DEFOCUS ON', '3': 'DEFOCUS OFF',
+                        '4': 'FLIP ON',    '5': 'FLIP OFF'}
+msg = Message()
 
 @app.route('/')
 @app.route('/<value>')
-def index(value=0):
-    print('value from button is: {}'.format(value))
-    return render_template("index.html", msg=msg)
+def index(value=None):
+    print('value from button press: {}'.\
+          format(msg.mapping[value]))
+    return render_template("index.html", buttons=buttons)
 
 if __name__ == '__main__':
     
