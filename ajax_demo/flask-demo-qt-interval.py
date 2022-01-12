8d305810887f433312862fba1ce1bf77f7524716
wesmith <smiwarsky@gmail.com>
2022-01-12 06:25:19 -0700

    progress in getting the ajax function to get data to the flask back-end

--- ajax_demo/flask-demo-qt-interval.py
+++ ajax_demo/flask-demo-qt-interval.py
@@ -15,6 +15,8 @@ from datetime import datetime
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
+''' # WS turn off db stuff
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,
                                                                     'qtdata.db')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
@@ -38,19 +40,37 @@ class Store_QTc_data(db.Model):
         self.heart_rate = heart_rate
         self.QT = QT
         self.sex = sex
+'''
 
 @app.route('/')
 def index():
-    if not os.path.exists(os.path.join(basedir, 'qtdata.db')):
-        db.create_all()
+    print('\n\nhello from index()\n\n')  # WS
+    #if not os.path.exists(os.path.join(basedir, 'qtdata.db')):
+    #    db.create_all()
     return render_template('index.html')
+
+
+@app.route('/WS_test', methods=['POST', 'GET'])
+def test():
+    print('\n\nin /WS_test route')
+    print('\n\nrequest = {}\n\n'.format(request.args))
+    #for j, k in request.args.get('key').iteritems:
+    #    print(j, k)
+    #print(dd)
+    return jsonify(output='it worked!')  # this works
+    #output = request
+    #return output
 
 @app.route('/process_qtc', methods=['POST', 'GET'])
 def process_qt_calculation():
+    print('\n\nhello from process_qt_calculation()\n\n')  # WS
+
     if request.method == "POST":
         qtc_data = request.get_json()
-        print('\n\nhello from /process_qtc\n\n')  # WS
+
         print('\n\nqtc_data: {}\n\n'.format(qtc_data)) # WS
+
+        ''' # WS turn off all db stuff
         db.session.add(Store_QTc_data(qtc_data[0]['QTc'],
                                       qtc_data[1]['prolonged'],
                                       qtc_data[2]['HR'],
@@ -58,9 +78,13 @@ def process_qt_calculation():
                                       qtc_data[4]['Sex']))
         db.session.commit()
         rows = db.session.query(Store_QTc_data).count()
+        '''
+    rows = 5 # WS
     results = {'rows': rows}
     return jsonify(results)
 
+
 if __name__ == "__main__":
+
     app.run(debug=True)
 
