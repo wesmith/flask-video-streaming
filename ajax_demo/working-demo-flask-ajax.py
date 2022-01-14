03cac2ece6cc3f50223b50f68a403ecc4c87ebff
wesmith <smiwarsky@gmail.com>
2022-01-14 02:01:57 -0700

    success: buttons and adder working in demo

--- ajax_demo/working-demo-flask-ajax.py
+++ ajax_demo/working-demo-flask-ajax.py
@@ -14,9 +14,22 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+default_button = ['DEFAULT', 0]
+
+buttons = {'INVERT': {'on': 1, 'off': 2},
+           'DEFOCUS':{'on': 3, 'off': 4},
+           'FLIP':   {'on': 5, 'off': 6}}
+
+button_vals = {'0': 'DEFAULT', 
+               '1': 'INVERT ON',  '2': 'INVERT OFF',
+               '3': 'DEFOCUS ON', '4': 'DEFOCUS OFF',
+               '5': 'FLIP ON',    '6': 'FLIP OFF'}
+
 @app.route('/')
 def index():
-    return render_template('working-demo-index.html')
+    return render_template('working-demo-index.html',
+                           default=default_button,
+                           buttons=buttons)
 
 @app.route('/WS_test', methods=['POST', 'GET'])
 def test():
@@ -29,8 +42,16 @@ def test():
                        format(res, datetime.now()))
     else:
         print('\n\nrequest method was not POST\n\n')
-        return jsonify(output='POST method not used')  
-
+        return jsonify(output='POST method not used')
+    
+@app.route('/button_result', methods=['POST', 'GET'])
+def button_result():
+    #print('\n\n IN BUTTON_RESULT \n\n')
+    if request.method == "POST":
+        result_txt = request.form['button_value']
+        print('button value: {}'.format(result_txt))
+        return jsonify(output=button_vals[result_txt])
+        #return ''
 
 if __name__ == "__main__":
 
