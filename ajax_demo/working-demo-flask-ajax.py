634d9dbac4f27d40644ba22e5e1f7a39782d564f
wesmith <smiwarsky@gmail.com>
2022-01-15 02:40:53 -0700

    modified buttons and can now get blur-kernel size: put in app.py

--- ajax_demo/working-demo-flask-ajax.py
+++ ajax_demo/working-demo-flask-ajax.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python3
 
-# bare-bones-flask-ajax.py
-# WESmith 01/12/22
+# working-demo-flask-ajax.py
+# WESmith 01/15/22
 # created a bare-bones example of flask and ajax
 
 
-#import os
 from flask import Flask, render_template, request, jsonify
-#from flask import url_for, session
 from datetime import datetime
-
-#basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-default_button = ['DEFAULT', 0]
+default_button = ['DEFAULT_VALUES', 0]
 
-buttons = {'INVERT': {'on': 1, 'off': 2},
-           'DEFOCUS':{'on': 3, 'off': 4},
-           'FLIP':   {'on': 5, 'off': 6}}
+invert = {'INVERT_COLORS': {'YES': 10, 'NO': 11}}
+flip   = {'FLIP_IMAGE':{'HORIZ': 20, 'VERT': 21,
+                        '180_DEG': 22}}
+gray   = {'GRAYSCALE': {'YES': 30, 'NO': 31}}
+blur   = {'BLUR':      {'TURN_OFF': 41}}
 
-button_vals = {'0': 'DEFAULT', 
-               '1': 'INVERT ON',  '2': 'INVERT OFF',
-               '3': 'DEFOCUS ON', '4': 'DEFOCUS OFF',
-               '5': 'FLIP ON',    '6': 'FLIP OFF'}
+button_vals  = {'0': 'DEFAULT',
+               '10': 'INVERT ON',  '11': 'INVERT OFF',
+               '20': 'FLIP HORIZ', '21': 'FLIP VERT',
+               '22': 'FLIP 180 DEG',
+               '30': 'GRAYSCALE',   '31': 'COLOR',
+               '41': 'NO BLURRING', '42': 'BLURRING ON'}
 
 @app.route('/')
 def index():
     return render_template('working-demo-index.html',
                            default=default_button,
-                           buttons=buttons)
+                           invert=invert,
+                           flip=flip,
+                           gray=gray,
+                           blur=blur)
 
 @app.route('/WS_test', methods=['POST', 'GET'])
 def test():
@@ -46,12 +49,20 @@ def test():
     
 @app.route('/button_result', methods=['POST', 'GET'])
 def button_result():
-    #print('\n\n IN BUTTON_RESULT \n\n')
     if request.method == "POST":
         result_txt = request.form['button_value']
         print('button value: {}'.format(result_txt))
-        return jsonify(output=button_vals[result_txt])
-        #return ''
+
+        # WS '999' is the symbol to indicate blurring-kernel size input
+        if result_txt[-3:] == '999':
+            val = result_txt[:-3] # strip off the symbol
+            print('val: {}'.format(val))
+            ttt = 'Blurring with kernel size {} x {}'.format(val, val)
+            print(ttt)
+            return jsonify(output=ttt)
+        else:
+            return jsonify(output=button_vals[result_txt])
+
 
 if __name__ == "__main__":
 
